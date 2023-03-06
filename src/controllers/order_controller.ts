b262ee7b5d75abe39c9153e3b379ddcfef9f343b
MohammadMzien2 <mohammadlu281@gmail.com>
2023-03-06 23:38:05 +0100

    added controllers for order and products

---
+++ src/controllers/order_controller.ts
@@ -0,0 +1,93 @@
+import Debug from "debug";
+import { Request, Response } from "express";
+import { validationResult } from "express-validator";
+import prisma from "../prisma";
+
+
+//Create a new debug instance
+const debug = Debug('prisma-boilerplate:I_AM_LAZY_AND_HAVE_NOT_CHANGED_THIS_😛')
+
+
+/**
+ * Get all orders
+ */
+export const index = async (req: Request, res: Response) => {
+	try {
+		const orders = await prisma.order.findMany({
+		})
+		res.send({
+			status: "success",
+			data: orders
+		})
+	}catch (err) {
+		debug("Error thrown when finding orders o%", err)
+		res.status(500).send({ status: "error", message: "something went wrong"})
+	}
+}
+
+/**
+ * Get a single order
+ */
+
+export const show = async(req: Request, res: Response) => {
+	const { oderId } = req.params
+	try{
+		const order = await prisma.order.findUniqueOrthrow({
+			where: {
+				id: Number(oderId)
+			}, include: {
+				order_items: true,
+			}
+		})
+		res.send({
+			status: "success",
+			data:order,
+		})
+	}catch(err){
+		debug("Thrown error something is wrong o%", req.params, err)
+		res.status(500).send({status: "error", message: "something is wrong"})
+	}
+}
+
+
+/**
+ * Create a order
+ */
+
+export const store = async (req: Request, res: Response) => {
+	const validationErros = validationResult(req)
+	if(!validationErros.isEmpty()){
+		return res.status(400).send({
+			status: "fail",
+			data: validationErros.array(),
+		})
+	}
+	try{
+		const{ order_items, customer_first_name, customer_last_name, customer_address, customer_postcode, customer_city, customer_email, customer_phone, order_total } = req.body
+		const order = await prisma.order.create({
+			data: {
+				customer_first_name,
+				customer_last_name,
+				customer_address,
+				customer_postcode,
+				customer_city,
+				customer_email,
+				customer_phone,
+				order_total,
+				order_items: {
+					create: order_items
+				},
+			},
+			include: {
+				order_items: true,
+			}
+		})
+		res.send({
+			status: "success",
+			data: order,
+		})
+	}catch(err){
+		debug("Error thrown when creating a order o% o%", err)
+		res.status(500).send({status: "error", message: "something went wrong"})
+	}
+}
---
+++ src/controllers/product_controller.ts
@@ -0,0 +1,81 @@
+import Debug from "debug";
+import { Request, Response } from "express";
+import { validationResult } from "express-validator";
+import prisma from "../prisma";
+
+
+//Create a new debug instance
+const debug = Debug('prisma-boilerplate:I_AM_LAZY_AND_HAVE_NOT_CHANGED_THIS_😛')
+
+
+/**
+ * Get all products
+ */
+export const index = async (req: Request, res: Response) => {
+	try{
+		const products = await prisma.product.findMany({
+		})
+		res.send({
+			status: "success",
+			data:products
+		})
+	}catch(err){
+		debug("Error thrown when finding books", err)
+		res.status(500).send({status: "error", message: "Something went wrong" })
+	}
+}
+
+/**
+ * Get a single product
+ */
+export const show = async (req: Request, res:Response) => {
+	const { productId } = req.params
+	try{
+		const product = await prisma.product.findUniqueOrthrow({
+			where: {
+				id: Number(productId)
+			}
+		})
+		res.send({
+			status: "success",
+			data: product,
+		})
+	}catch(err){
+		debug("Thrown error when something didn't go as planned %o %o", req.params, err)
+		res.send(500).send({status: "error", message: "something is false"})
+	}
+}
+
+/**
+ * Create a product
+ */
+export const store = async (req: Request, res: Response) => {
+	const validationErros = validationResult(req)
+	if(!validationErros.isEmpty()){
+		return res.status(400).send({
+			status: "fail",
+			data: validationErros.array(),
+		})
+	}
+	try{
+		const { name, description, price, images, stock_status, stock_quantity, on_sale,} = req.body
+		const product = await prisma.product.create({
+			data: {
+				name,
+				description,
+				price,
+				images,
+				stock_status,
+				stock_quantity,
+				on_sale
+			}
+		})
+		res.send({
+			status: "success",
+			data: product,
+		})
+	}catch (err){
+		debug("Error thrown when creating a product %o %o", req.body, err)
+		res.send(500).send({ status: "error", message: "something went wrong"})
+	}
+}

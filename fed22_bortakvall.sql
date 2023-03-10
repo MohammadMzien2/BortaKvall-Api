b5c9889cf0ec70a2a21e8bb7d8428a5d702efe45
MohammadMzien2 <mohammadlu281@gmail.com>
2023-03-10 10:16:43 +0100

    added mysqk filen

--- fed22_bortakvall.sql
+++ fed22_bortakvall.sql
@@ -2,10 +2,10 @@
 -- version 4.9.5
 -- https://www.phpmyadmin.net/
 --
--- Värd: localhost:3306
--- Tid vid skapande: 06 mars 2023 kl 23:00
--- Serverversion: 5.7.24
--- PHP-version: 7.4.1
+-- Host: localhost:3306
+-- Generation Time: Mar 10, 2023 at 09:15 AM
+-- Server version: 5.7.24
+-- PHP Version: 7.4.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,41 +19,41 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Databas: `fed22_bortakvall`
+-- Database: `fed22_bortakvall`
 --
 
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `order`
+-- Table structure for table `order`
 --
 
 CREATE TABLE `order` (
   `id` int(10) UNSIGNED NOT NULL,
   `customer_first_name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `customer_last_name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `customer_address` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `customer_postcode` varchar(6) COLLATE utf8mb4_unicode_ci NOT NULL,
   `customer_city` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `customer_email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `customer_phone` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `order_total` int(11) NOT NULL,
-  `customer_address` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL
+  `order_total` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumpning av Data i tabell `order`
+-- Dumping data for table `order`
 --
 
-INSERT INTO `order` (`id`, `customer_first_name`, `customer_last_name`, `customer_postcode`, `customer_city`, `customer_email`, `customer_phone`, `order_total`, `customer_address`) VALUES
-(8, 'Johan', 'Nordström', '224 79', 'Malmö', 'jn@thehiveresistance.com', '0736352383', 52, 'Drottninggatan 4B'),
-(9, 'Mohammad', 'mzien', '262 31', 'Helsingborg', 'mohammad@gmail.com', '', 52, 'skolgatan 30'),
-(10, 'gustav', 'hannsson', '252 25', 'Lund', 'Gustav@hotmail.com', '', 52, 'gustav-gata 1A'),
-(11, 'Douglas', 'andersson', '262 45', 'Ängelhom', 'Douglas@gmail.com', '07336636321', 52, 'rönnegata 20');
+INSERT INTO `order` (`id`, `customer_first_name`, `customer_last_name`, `customer_address`, `customer_postcode`, `customer_city`, `customer_email`, `customer_phone`, `order_total`) VALUES
+(3, 'Johan', 'Nordström', 'Drottninggatan 4B', '224 79', 'Malmö', 'jn@thehive.com', '0736342591', 52),
+(7, 'Mohammad', 'mzien', 'skolgatan 33', '26231', 'ängelholm', 'mohammad@gamil.com', '073634621', 50),
+(8, 'gustav', 'hansson', 'malmö 33', '22479', 'malmö', 'gustav@gmail.com', '', 40),
+(9, 'douglas', 'andersson', 'ängelholm 1A', '262 45', 'helsingborg', 'douglas@gmail.com', '', 20);
 
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `orderitem`
+-- Table structure for table `orderitem`
 --
 
 CREATE TABLE `orderitem` (
@@ -65,10 +65,19 @@ CREATE TABLE `orderitem` (
   `order_id` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `orderitem`
+--
+
+INSERT INTO `orderitem` (`id`, `product_id`, `qty`, `item_price`, `item_total`, `order_id`) VALUES
+(6, 1, 10, 20, 24, 7),
+(7, 1, 20, 30, 25, 8),
+(8, 3, 5, 15, 20, 9);
+
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `product`
+-- Table structure for table `product`
 --
 
 CREATE TABLE `product` (
@@ -76,27 +85,27 @@ CREATE TABLE `product` (
   `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `price` int(10) UNSIGNED NOT NULL,
-  `on_sale` tinyint(1) DEFAULT NULL,
   `images` json NOT NULL,
   `stock_status` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `stock_quantity` int(10) UNSIGNED NOT NULL
+  `stock_quantity` int(10) UNSIGNED NOT NULL,
+  `on_sale` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumpning av Data i tabell `product`
+-- Dumping data for table `product`
 --
 
-INSERT INTO `product` (`id`, `name`, `description`, `price`, `on_sale`, `images`, `stock_status`, `stock_quantity`) VALUES
-(8, 'Gott & Blandat Giants', '<p>En mix av lakrits och gelé med fruktsmak</p>\n<p>Innehållsförteckning: Socker, glukossirap, glukos-fruktossirap, stärkelse, VETEMJÖL, melass, syra (citronsyra), fuktighetsbevarande medel (sorbitoler, glycerol), lakritsextrakt, salt, vegetabiliska oljor (kokos, palm), aromer, färgämnen (E153, E120, E100, E141), ytbehandlingsmedel (bivax), stabiliseringsmedel (E471).</p>\n<p><em>Alla priser är per skopa.</em></p>\n', 12, 0, '{\"large\": \"/storage/products/1997509.png\", \"thumbnail\": \"/storage/products/thumbnails/1997509-300x300.png\"}', 'instock', 5),
-(9, 'Banana Bubs', '<p>Banan/gräddkola</p>\n<p>Innehållsförteckning: Glukos-fruktossirap, socker, majsstärkelse, vatten, surhetsreglerande medel (äppelsyra, natriumcitrat), potatisprotein, aromämnen, färgämnen: (E150d, E100), kokosolja, ytbehandlingsmedel (karnaubavax).</p>\n<p><em>Alla priser är per skopa.</em></p>\n', 8, 0, '{\"large\": \"/storage/products/156622.png\", \"thumbnail\": \"/storage/products/thumbnails/156622-300x300.png\"}', 'instock', 8),
-(10, 'Banana Splits', '<p>Fyllig vitchoklad med smak av kola krispig banan.</p>\n<p>Innehållsförteckning: Socker, kakaosmör, torkad banan, HELMJÖLKSPULVER, mjölksocker(LAKTOS), SKUMMJÖLKSPULVER, emulgeringsmedel E322 (SOJALECITIN), naturliga aromer, ytbehandlingsmedel (E414, E904).</p>\n<p>Kan innehålla spår av MANDEL, NÖTTER och VETE.</p>\n<p><em>Alla priser är per skopa.</em></p>\n', 8, 0, '{\"large\": \"/storage/products/3827741.png\", \"thumbnail\": \"/storage/products/thumbnails/3827741-300x300.png\"}', 'instock', 6),
-(11, 'S-Märke Colasura', '<p>Sursockrad gelé med colasur.</p>\n<p>Innehållsförteckning: Socker, glukos, gelatin (nöt), surhetsreglerande medel(äppelsyra, natriumcitrat), aromer, färgämnen(karmin, sockerkulör).</p>\n<p><em>Alla priser är per skopa.</em></p>\n', 5, 0, '{\"large\": \"/storage/products/s-marke-cola.jpg\", \"thumbnail\": \"/storage/products/thumbnails/s-marke-cola-300x300.jpg\"}', 'instock', 3),
-(12, 'Ferrari päron', '<p>Supersmarrig och snabb Ferraribil med smak av päron. </p>\n<p>Ingredienser: Socker, glukossirap, modifierad majsstärkelse, gelatin, surhetsreglerande medel (citronsyra), aromämne, färgämne (klorofyll-kopparkomplex, kurkumin), vegetabilisk olja (kokos, raps), ytbehandlingsmedel (karnaubavax). </p>\n<p><em>Alla priser är per skopa.</em></p>\n', 10, 0, '{\"large\": \"/storage/products/Ferrariparon.jpg\", \"thumbnail\": \"/storage/products/thumbnails/Ferrariparon-300x300.jpg\"}', 'instock', 5);
+INSERT INTO `product` (`id`, `name`, `description`, `price`, `images`, `stock_status`, `stock_quantity`, `on_sale`) VALUES
+(1, 'Nöt-Créme', '<p>Hasselnötskräm</p>\n<p>Innehållsförteckning: Socker, vegetabiliska oljor och fetter (shea och palm i varierande proportion), HASSELNÖTTER, JORDNÖTTER, kakao, naturlig vaniljarom, emulgator (solroslecitin).</p>\n<p><em>Alla priser är per skopa.</em></p>\n', 7, '{\"large\": \"/storage/products/200549.png\", \"thumbnail\": \"/storage/products/thumbnails/200549-300x300.png\"}', 'instock', 3, 0),
+(2, 'Gott & Blandat Giants', '<p>Banan/gräddkola</p>\n<p>Innehållsförteckning: Glukos-fruktossirap, socker, majsstärkelse, vatten, surhetsreglerande medel (äppelsyra, natriumcitrat), potatisprotein, aromämnen, färgämnen: (E150d, E100), kokosolja, ytbehandlingsmedel (karnaubavax).</p>\n<p><em>Alla priser är per skopa.</em></p>\n', 12, '{\"large\": \"/storage/products/156622.png\", \"thumbnail\": \"/storage/products/thumbnails/156622-300x300.png\"}', 'instock', 5, 0),
+(3, 'Banana Splits', '<p>Banan/gräddkola</p>\n<p>Innehållsförteckning: Glukos-fruktossirap, socker, majsstärkelse, vatten, surhetsreglerande medel (äppelsyra, natriumcitrat), potatisprotein, aromämnen, färgämnen: (E150d, E100), kokosolja, ytbehandlingsmedel (karnaubavax).</p>\n<p><em>Alla priser är per skopa.</em></p>\n', 8, '{\"large\": \"/storage/products/3827741.png\", \"thumbnail\": \"/storage/products/thumbnails/3827741-300x300.png\"}', 'instock', 6, 0),
+(4, 'S-Märke Colasura', '<p>Sursockrad gelé med colasur.</p>\n<p>Innehållsförteckning: Socker, glukos, gelatin (nöt), surhetsreglerande medel(äppelsyra, natriumcitrat), aromer, färgämnen(karmin, sockerkulör).</p>\n<p><em>Alla priser är per skopa.</em></p>\n\' (karnaubavax).</p>\n<p><em>Alla priser är per skopa.</em></p>\n', 8, '{\"large\": \"/storage/products/s-marke-cola.jpg\", \"thumbnail\": \"/storage/products/thumbnails/s-marke-cola-300x300.jpg\"}', 'instock', 3, 0),
+(5, 'Ferrari päron', '<p>Supersmarrig och snabb Ferraribil med smak av päron. </p>\n<p>Ingredienser: Socker, glukossirap, modifierad majsstärkelse, gelatin, surhetsreglerande medel (citronsyra), aromämne, färgämne (klorofyll-kopparkomplex, kurkumin), vegetabilisk olja (kokos, raps), ytbehandlingsmedel (karnaubavax). </p>\n<p><em>Alla priser är per skopa.</em></p>\n', 10, '{\"large\": \"/storage/products/Ferrariparon.jpg\", \"thumbnail\": \"/storage/products/thumbnails/Ferrariparon-300x300.jpg\"}', 'instock', 5, 0);
 
 -- --------------------------------------------------------
 
 --
--- Tabellstruktur `_prisma_migrations`
+-- Table structure for table `_prisma_migrations`
 --
 
 CREATE TABLE `_prisma_migrations` (
@@ -111,24 +120,24 @@ CREATE TABLE `_prisma_migrations` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumpning av Data i tabell `_prisma_migrations`
+-- Dumping data for table `_prisma_migrations`
 --
 
 INSERT INTO `_prisma_migrations` (`id`, `checksum`, `finished_at`, `migration_name`, `logs`, `rolled_back_at`, `started_at`, `applied_steps_count`) VALUES
-('57ff0e67-afec-4857-8d1f-c75cb1a347a5', '8f1e85125303501edda8efd980ffda165cf9edda4236d3b4589cadbd26a8535c', '2023-02-08 11:06:28.328', '20230208110627_schema_update', NULL, NULL, '2023-02-08 11:06:27.953', 1);
+('5b33a192-f187-412f-9142-15430237d08a', 'fdfebda0f16b9a2921bcecfce3095f397da0413384c2713500ca94c604e83929', '2023-03-10 07:43:24.687', '20230310074324_init', NULL, NULL, '2023-03-10 07:43:24.317', 1);
 
 --
--- Index för dumpade tabeller
+-- Indexes for dumped tables
 --
 
 --
--- Index för tabell `order`
+-- Indexes for table `order`
 --
 ALTER TABLE `order`
   ADD PRIMARY KEY (`id`);
 
 --
--- Index för tabell `orderitem`
+-- Indexes for table `orderitem`
 --
 ALTER TABLE `orderitem`
   ADD PRIMARY KEY (`id`),
@@ -136,45 +145,45 @@ ALTER TABLE `orderitem`
   ADD KEY `OrderItem_order_id_fkey` (`order_id`);
 
 --
--- Index för tabell `product`
+-- Indexes for table `product`
 --
 ALTER TABLE `product`
   ADD PRIMARY KEY (`id`);
 
 --
--- Index för tabell `_prisma_migrations`
+-- Indexes for table `_prisma_migrations`
 --
 ALTER TABLE `_prisma_migrations`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT för dumpade tabeller
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT för tabell `order`
+-- AUTO_INCREMENT for table `order`
 --
 ALTER TABLE `order`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
--- AUTO_INCREMENT för tabell `orderitem`
+-- AUTO_INCREMENT for table `orderitem`
 --
 ALTER TABLE `orderitem`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- AUTO_INCREMENT för tabell `product`
+-- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- Restriktioner för dumpade tabeller
+-- Constraints for dumped tables
 --
 
 --
--- Restriktioner för tabell `orderitem`
+-- Constraints for table `orderitem`
 --
 ALTER TABLE `orderitem`
   ADD CONSTRAINT `OrderItem_order_id_fkey` FOREIGN KEY (`order_id`) REFERENCES `order` (`id`) ON UPDATE CASCADE,
